aa637b1b62fe592cdfba4be4391d66ba6b958170
MattSedmak <sedmakmatt@gmail.com>
2024-08-26 13:05:20 +0200

    updated readme

--- app/api/endpoints/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from '@/prisma/client';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const endpoints = await prisma.endpoint.findMany();
-
-  return NextResponse.json(endpoints);
-}
